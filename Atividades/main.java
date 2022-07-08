b7c657de66416729f060740b5ae38e75a4a19c2f
luanmarcal <luan.marcal@padolabs.com.br>
2022-07-08 17:34:14 -0300

    :books: atividade-slide01

---
+++ Atividades/main.java
@@ -0,0 +1,13 @@
+public class main {
+    public static void main(String[] args) {
+        String product1 = "Computador";
+        String product2 = "Celular";
+
+        double price1 = 2100.0;
+        double price2 = 650.50;
+
+        System.out.printf("%nProdutos:");
+        System.out.printf("%n%s, com preço de $%.2f", product1, price1);
+        System.out.printf("%n%s, com preço de $%.2f%n", product2, price2);
+    }
+}
